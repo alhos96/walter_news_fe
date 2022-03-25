c9ea668a6d7a0a1cc646ac50cd0510284562b197
Alija Hodzic <hodzic.alija5@gmail.com>
2022-03-25 09:40:32 +0100

    create card element

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,5 +1,22 @@
-function Card() {
-  return <div>Card</div>;
+import noImage from "../../assets/images/noImage.png";
+
+function Card({ imgUrl, title, description }) {
+  const showDefaultImage = (e) => {
+    e.target.src = noImage; // if url link is broken show default image
+  };
+
+  return (
+    <div className="max-w-xs min-w-[320px] min-h-[180px] max-h-[180px] relative">
+      <img
+        alt="image"
+        className="brightness-75 min-w-full object-contain"
+        src={imgUrl || noImage /* Undefined url won't throw error. In that case show default image right upon render.*/}
+        onError={(e) => showDefaultImage(e)}
+      />
+      <h1 className="text-lg font-medium leading-6 absolute top-2 p-1">{title}</h1>
+      <p className="text-xs leading-4 absolute bottom-2 p-1 pl-2">{description}</p>
+    </div>
+  );
 }
 
 export default Card;
