9a27b4427be67bd4c43c15c663df2c223bab8c65
Alija Hodzic <hodzic.alija5@gmail.com>
2022-03-26 10:11:51 +0100

    Recieve articles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import { Header, Main } from "./components";
-import Spinner from "./components/spinner/Spinner";
+import { Header, Main, Spinner } from "./components";
+import { handleApiCall, config } from "./helpers";
+import { topHeadlinesRecieved } from "./store/newsSlice";
 
 function App() {
-  const [loading, setLoading] = useState(true);
+  // helpers
+  const dispatch = useDispatch();
+  const { endpoints, get } = config;
+
+  // global state
+  const resultsLoading = useSelector((state) => state.news.resultsLoading);
+
+  // local state
+  const [showAmount, setShowAmount] = useState(3); // amount of articles to ask for
+
+  // functions
+  const handleLoadMoreClick = () => {
+    setShowAmount((prev) => prev + 3);
+  };
+
+  // sideffects
+  useEffect(() => {
+    handleApiCall(`${endpoints.topHeadlines}&pageSize=${showAmount}`, get, dispatch, topHeadlinesRecieved);
+  }, [showAmount]);
 
   return (
     <div className="App relative">
       <Header />
 
-      {loading ? <Spinner /> : <Main />}
+      {resultsLoading ? <Spinner /> : <Main />}
     </div>
   );
 }
