173596f7b724af38bd1b6e7f30f4d1b13bb64a92
Alija Hodzic <hodzic.alija5@gmail.com>
2022-03-26 10:15:20 +0100

    iniital functionality of main component with articles

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,10 +1,30 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+
 import "./main.css";
 import Card from "../card/Card";
+import Button from "../button/Button";
 
 function Main() {
+  // global state
+  const topHeadlines = useSelector((state) => state.news.topHeadlines);
+  const searchedHeadlines = useSelector((state) => state.news.searchedHeadlines);
+
+  // this value will serve as boolean to render upon
+  const showTopHeadlines = topHeadlines.length;
+  const showSearchedHeadlines = searchedHeadlines.length;
+
+  const showCards = (data) => {
+    return data.map((article, index) => {
+      return <Card key={article?.publishedAt} imgUrl={article?.urlToImage} title={article?.title} description={article?.description} />;
+    });
+  };
+
   return (
     <div className="main bg-transparent absolute top-28">
-      <Card />
+      {showCards((showTopHeadlines && topHeadlines) || (showSearchedHeadlines && searchedHeadlines))}
+
+      {showTopHeadlines && <Button handler={() => console.log("jabuka")} text={"Load More"} />}
     </div>
   );
 }
