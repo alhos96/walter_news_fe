6ae1484fb4add522e45cfb71f3945f103b3a3eb6
Alija Hodzic <hodzic.alija5@gmail.com>
2022-03-26 14:53:11 +0100

    add search field

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,17 +2,41 @@ import { useState, useEffect } from "react";
 
 import "./header.css";
 import logo from "../../assets/images/logo.svg";
+import searchIcon from "../../assets/images/search-icon.svg";
 import SlideFromTop from "../animations/SlideFromTop";
 
 function Header() {
+  // local state
   const [isVisible, setIsVisible] = useState(false);
+  const [buttonOpacity, setButtonOpacity] = useState(50);
 
+  // side effects
   useEffect(() => {
     setIsVisible(true);
   }, []);
+
   return (
-    <div className="header p-5 h-48">
+    <div className="header p-5 h-60 relative">
       <SlideFromTop isVisible={isVisible} children={<img src={logo} alt="logo" className="m-auto" />} />
+
+      <div
+        tabIndex="0"
+        onFocus={(e) => {
+          e.target.placeholder = "";
+          setButtonOpacity(100); // make button animate with border
+        }}
+        onBlur={(e) => {
+          e.target.placeholder = "Search...";
+          setButtonOpacity(50);
+        }}
+        className="search-wrapp m-auto mt-5 max-w-[400px] relative"
+      >
+        <input placeholder="Search..." className="search-input bg-transparent w-full" type="text" />
+
+        <button className={`search-button absolute right-0 bottom-0 opacity-${buttonOpacity}`}>
+          <img src={searchIcon}></img>
+        </button>
+      </div>
     </div>
   );
 }
